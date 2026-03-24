e68aa876c2609c5f0f31485a0a95794c74c120ba
davo <170838562+daftvoid@users.noreply.github.com>
2026-03-24 20:56:32 +0100

    feat(config): add config.ts

--- src/engine/bootstrap.ts
+++ src/engine/bootstrap.ts
@@ -1,14 +1,15 @@
 import type {Game} from "./core/game.ts";
 import {Renderer} from "./core/renderer.ts";
 import {Input} from "./core/input.ts";
+import {config} from "./config.ts";
 
 export function createGamePage(GameClass: new () => Game) {
     console.log("test")
 
     const canvas = document.getElementById("gamecanvas") as HTMLCanvasElement;
 
-    canvas.width = 240
-    canvas.height = 180
+    canvas.width = config.canvas_width
+    canvas.height = config.canvas_height
 
     const renderer = new Renderer(canvas);
     const input = new Input();
---
+++ src/engine/config.ts
@@ -0,0 +1,4 @@
+export const config = {
+    canvas_width: 240,
+    canvas_height: 180,
+}
