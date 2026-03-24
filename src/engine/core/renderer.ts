d76daa4d05d31f39664e740b04473cf895624875
davo <170838562+daftvoid@users.noreply.github.com>
2026-03-24 15:40:09 +0100

    fix(canvas): made canvas pixelated

--- src/engine/core/renderer.ts
+++ src/engine/core/renderer.ts
@@ -3,6 +3,8 @@ export class Renderer {
 
     constructor(private canvas: HTMLCanvasElement) {
         this.ctx = canvas.getContext("2d")!;
+
+        this.ctx.imageSmoothingEnabled = false
     }
 
     clear() {
