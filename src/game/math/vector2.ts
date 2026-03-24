83b43a0c362d929aba03503361898f36576a0107
davo <170838562+daftvoid@users.noreply.github.com>
2026-03-24 10:22:03 +0100

    feat(math): add vector2 interface

---
+++ src/game/math/vector2.ts
@@ -0,0 +1,14 @@
+export interface Vector2Like {
+    x: number;
+    y: number;
+}
+
+export class Vector2 implements Vector2Like {
+    x: number;
+    y: number;
+
+    constructor(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+    }
+}
