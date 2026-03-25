12cd532ac5b24e1ab62df3cab133cfe9f01a5724
davo <170838562+daftvoid@users.noreply.github.com>
2026-03-25 19:08:26 +0100

    feat(input): add eventhandler to `Input`

--- src/engine/core/input.ts
+++ src/engine/core/input.ts
@@ -1,12 +1,29 @@
 export class Input {
     keys = new Set<string>();
 
+    private keyDownHandlers: ((key: string) => void)[] = [];
+    private keyUpHandlers: ((key: string) => void)[] = [];
+
     constructor() {
-        window.addEventListener("keydown", e => this.keys.add(e.key));
-        window.addEventListener("keyup", e => this.keys.delete(e.key));
+        window.addEventListener("keydown", e => {
+            this.keys.add(e.key)
+            this.keyDownHandlers.forEach(fn => fn(e.key))
+        });
+        window.addEventListener("keyup", e => {
+            this.keys.delete(e.key)
+            this.keyUpHandlers.forEach(fn => fn(e.key))
+        });
     }
 
     isDown(key: string) {
         return this.keys.has(key);
+    }
+
+    onKeyDown(handler: (key: string) => void) {
+        this.keyDownHandlers.push(handler);
+    }
+
+    onKeyUp(handler: (key: string) => void) {
+        this.keyUpHandlers.push(handler);
     }
 }
