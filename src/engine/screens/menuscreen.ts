b146b78932804e47f3ad62451abb0cd63e6e648e
davo <170838562+daftvoid@users.noreply.github.com>
2026-03-25 09:19:18 +0100

    feat(menu): add menuscreen

---
+++ src/engine/screens/menuscreen.ts
@@ -0,0 +1,24 @@
+import {Screen} from "./screen.ts";
+import type {Renderer} from "../core/renderer.ts";
+import type {Input} from "../core/input.ts";
+import {config} from "../config.ts";
+
+export class MenuScreen extends Screen {
+    constructor(private onStart: () => void, private img?: HTMLImageElement) {
+        super();
+    }
+
+    update(delta: number, input: Input): void {
+        if (input.isDown("Enter")) {
+            this.onStart()
+        }
+    }
+    render(r: Renderer) {
+        if (this.img) r.drawImage(this.img, 0, 0, config.canvas_width, config.canvas_height);
+
+        r.advancedText("PRESS    START", config.canvas_width / 2, config.canvas_height / 2, "#fff", {
+            textBaseline: "middle",
+            textAlign: "center",
+        })
+    }
+}
