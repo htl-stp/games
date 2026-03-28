24e2faec6b3f1a2540be5b3d81624f17bcdcc661
KilianSteigberger <20230254@htlstp.at>
2026-03-28 16:41:38 +0100

    feat(ttt): implemented Target class

--- src/games/test-the-tables/game.ts
+++ src/games/test-the-tables/game.ts
@@ -1,6 +1,7 @@
 import {Entity} from "../../engine/entity/entity.ts";
 import {config} from "../../engine/config.ts";
 import  {type Renderer} from "../../engine/core/renderer.ts";
+import {Scene} from "../../engine/scenes/scene.ts";
 
 type TableStatus = "home" | "moving" | "locked";
 
@@ -42,4 +43,36 @@ class HomeSlot extends Entity {
         r.drawRect(this.x,this.y,this.w,this.h, "rgba(255,255,255,0.2)")
         r.advancedText(this.tableId.toString(),this.x,this.y,config.theme.colors.black,{textAlign:"center",textBaseline:"middle"})
     }
+}
+
+class Target extends Entity {
+    public isFilled: boolean = false;
+
+    constructor(public id:number, x:number, y:number, w:number, h:number) {
+        super(x, y, w, h);
+    }
+    checkSnap(table:Table):boolean {
+        if(table.id !== this.id || this.isFilled){return false}
+
+        if(table.collidesWith(this)){
+            table.x = this.x;
+            table.y = this.y;
+            this.isFilled = true;
+            table.status = "locked";
+            return true;
+        }
+        return false;
+    }
+    render(r: Renderer) {
+        const color = this.isFilled? config.theme.colors.green : "rgba(255,255,255,0.2)";
+        r.drawRect(this.x,this.y,this.w,this.h,color)
+
+        r.advancedText(this.id.toString(),this.x, this.y, config.theme.colors.white,
+            {textAlign : "center", textBaseline : "middle"})
+    }
+}
+type GameState = "start" | "running" | "end"
+
+class GameScene extends Scene {
+
 }
