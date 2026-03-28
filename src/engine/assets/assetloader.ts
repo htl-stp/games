055c4e97160ad7405701176d0cedff1aa7da6110
davo <170838562+daftvoid@users.noreply.github.com>
2026-03-28 10:02:20 +0100

    feat(engine): add `Asset` and `AssetLoader`

---
+++ src/engine/assets/assetloader.ts
@@ -0,0 +1,13 @@
+import type {Asset} from "./asset.ts";
+
+export class AssetLoader {
+    private assets: Asset[] = []
+
+    add(asset: Asset): void {
+        this.assets.push(asset);
+    }
+
+    async loadAll() {
+        await Promise.all(this.assets.map(a => a.load()));
+    }
+}
